59ae9e3e8c947807872c1101076745a17e1423ae
David Smitmanis <davidsm@axis.com>
2025-02-17 11:40:15 +0100

    Disable more actix logs

--- src/main.rs
+++ src/main.rs
@@ -177,11 +177,8 @@ struct Opts {
 #[actix_web::main]
 async fn main() -> Result<()> {
     let opts = Opts::parse();
-    env_logger::Builder::from_env(
-        Env::default()
-            .default_filter_or("info,actix_server::builder=warn,actix_server::server=warn"),
-    )
-    .init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info,actix_server=warn"))
+        .init();
 
     let Some(project_dirs) = ProjectDirs::from("", "", "kypare") else {
         bail!("Failed to determine home directory");
